0226af3dd42b4c04e8c8b3a914d4c2b451707240
Andres Pardo <andrespardo5151@gmail.com>
2020-10-13 11:32:26 -0500

    Inicio curso php 2020

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+$num = 9;
+$lang = [
+    'es' => 'español',
+    'en' => 'imgles'
+];
+
+//Aritmetica 
+
+echo "Suma 2 + 2 " . ((int ) 2 + (int) 2 );
+echo "Resta 2 - 2 " . ((int ) 2 - (int) 2 );
+echo "Multiplicacion  2 * 2 " . 2 * 2 ;
+echo "Multiplicacion  2 * 2 " . 2 * 2 ;
+
