a4a1e1b69b5245909f86fd870e294b21dc65ddd8
Andres Pardo <andrespardo5151@gmail.com>
2020-10-14 10:41:56 -0500

    feat: sintaxis basica de php

--- index.php
+++ index.php
@@ -6,10 +6,11 @@ $lang = [
     'en' => 'imgles'
 ];
 
-//Aritmetica 
+//Aritmetica  
 
 echo "Suma 2 + 2 " . ((int ) 2 + (int) 2 );
 echo "Resta 2 - 2 " . ((int ) 2 - (int) 2 );
 echo "Multiplicacion  2 * 2 " . 2 * 2 ;
-echo "Divicion   2 * 2 " . 2 * 2 ;
-
+echo "Divicion   2 / 2 " . 2 / 2 ;
+echo "Modulo   2 % 2 " . 2 % 2 ;
+echo "Exponencial   2 ** 2 " . 2 ** 2 ;  
