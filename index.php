e6815cb08884579e0c1c1b29735f7d14681d445c
Andres Pardo <andrespardo5151@gmail.com>
2020-10-27 11:46:49 -0500

    feat:Revision de proyeccyo

--- index.php
+++ index.php
@@ -1,30 +1,10 @@
 <?php 
 
-$num = 9;
-$lang = [
-    'es' => 'español',
-    'en' => 'imgles'  
-];
+require __DIR__ . '/vendor/autoload.php';
 
-//Aritmetica  
-
-echo "Suma 2 + 2 " . ((int ) 2 + (int) 2 );
-echo "Resta 2 - 2 " . ((int ) 2 - (int) 2 );
-echo "Multiplicacion  2 * 2 " . 2 * 2 ;
-echo "Divicion   2 / 2 " . 2 / 2 ;
-echo "Modulo   2 % 2 " . 2 % 2 ;
-echo "Exponencial   2 ** 2 " . 2 ** 2 ;  
-
-// Comparacion == 
-// Igual ===, valor - tipo 9 === 9
-// Diferencias !=, valor 
-// Diferencias !==, valor - tipo 
-// <, >, <= >=
+echo lower('hola');
 
 
-// variables variables 
-$app = 'name';
-$name = 'platzi'
 
-echo $app; // platzi
-echo $$app; // platzi 
+
+
